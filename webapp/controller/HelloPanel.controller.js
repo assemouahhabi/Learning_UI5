e17786d8634674b8de1125157a6c5cd5397216ce
assemouahhabi <assemouahhabi@gmail.com>
2021-11-24 21:34:05 +0100

    'Formation_UI5'

---
+++ webapp/controller/HelloPanel.controller.js
@@ -0,0 +1,30 @@
+sap.ui.define(
+  ["sap/ui/core/mvc/Controller", "sap/m/MessageToast", "sap/ui/core/Fragment"],
+  function (Controller, MessageToast, Fragment) {
+    "use strict";
+    return Controller.extend("sap.ui.demo.walkthrough.controller.HelloPanel", {
+      onShowHello: function () {
+        var oBundle = this.getView().getModel("i18n").getResourceBundle();
+        var sRecipient = this.getView()
+          .getModel()
+          .getProperty("/recipient/name");
+
+        var sMsg = oBundle.getText("HelloMsg", [sRecipient]);
+        MessageToast.show(sMsg);
+      },
+      onOpenDialog: function () {
+        if (!this.pDialog) {
+          this.pDialog = this.loadFragment({
+            name: "sap.ui.demo.walkthrough.view.HelloDialog",
+          });
+        }
+      this.pDialog.then(function(oDialog){
+          oDialog.open()
+      })
+      },
+      onCloseDialog: function(){
+          this.byId("helloDialog").close();
+      }
+    });
+  }
+);
